905ecfad6ed4322808f9b860df197ae90cfb717b
RomainGuarinoni <romain.guar91@gmail.com>
2021-10-05 16:59:57 +0200

    feat(core): set up^font family

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/index.module.css';
+
+export default function Index() {
+  return (
+    <div>
+      <Head>
+        <link
+          href='https://fonts.googleapis.com/css2?family=Inter&display=optional'
+          rel='stylesheet'
+        />
+      </Head>
+    </div>
+  );
+}
