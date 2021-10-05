3a8e13f4b62bedfc271090f997938c4a5ce97677
RomainGuarinoni <romain.guar91@gmail.com>
2021-10-05 18:10:17 +0200

    feat(components): add game component

--- components/gameCard.tsx
+++ components/gameCard.tsx
@@ -1,5 +1,13 @@
 import style from '../styles/gameCard.module.css';
+import Image from 'next/image';
 
 export default function gameCard(): JSX.Element {
-  return <p>Game Card</p>;
+  return (
+    <div className={style.container}>
+      <h2>CSGO</h2>
+      <div className={style.image}>
+        <Image src={require('../assets/csgo.png')} />
+      </div>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,11 +14,29 @@ export default function Index(): JSX.Element {
           href='https://fonts.googleapis.com/css2?family=Inter&display=optional'
           rel='stylesheet'
         />
+        <title>Game wheel</title>
       </Head>
       <RunButton run={run} setRun={setRun} />
       <div className={styles.gameContainer}>
         <GameCard />
         <GameCard />
+        <GameCard />
+        <GameCard />
+
+        <GameCard />
+
+        <GameCard />
+
+        <GameCard />
+
+        <GameCard />
+
+        <GameCard />
+
+        <GameCard />
+
+        <GameCard />
+
         <GameCard />
       </div>
     </div>
